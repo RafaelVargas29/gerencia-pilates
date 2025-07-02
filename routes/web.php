04b2528543eb95f5b19231f1dbad13f48fa7718c
Rafael Vargas <rafaelrgsv@gmail.com>
2025-07-02 18:50:05 -0300

    creating routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+use App\Http\Controllers\AlunoController;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::get('/aluno', [AlunoController::class, 'index'])->name('aluno.index');
+Route::get('aluno/create', [AlunoController::class, 'create'])->name('aluno.create');
+Route::post('/aluno', [AlunoController::class, 'store'])->name('aluno.store');
+Route::get('/aluno/{aluno}', [AlunoController::class, 'show'])->name('aluno.show');
+Route::put('/aluno/{aluno}', [AlunoController::class, 'update'])->name('aluno.update');
+Route::delete('/aluno/{aluno}', [AlunoController::class, 'destroy'])->name('aluno.destroy');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
